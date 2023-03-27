e23aef26509e362f355bc311c40a03c57018ea8e
Frexom <hugom2003@sfr.fr>
2023-03-27 13:19:31 +0200

    Created project

---
+++ app/src/main/java/quoi/feur/yoyodroid/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package quoi.feur.yoyodroid.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
---
+++ app/src/main/java/quoi/feur/yoyodroid/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package quoi.feur.yoyodroid.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import quoi.feur.yoyodroid.data.LoginDataSource
+import quoi.feur.yoyodroid.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
